f04beafefbfd4cddb2f0d7986627f90765acbb9d
Вадим <73397467+FireVaDick@users.noreply.github.com>
2023-04-12 23:37:17 +0300

    localStorage

--- weather-app/src/provider/ThemeProvider.tsx
+++ weather-app/src/provider/ThemeProvider.tsx
@@ -1,15 +1,20 @@
 import { ReactNode, useState } from 'react';
 import { changeSccRootVariables } from '../model/ChangeSccRootVariables';
 import { Theme, ThemeContext } from '../context/ThemeContext';
+import { storage } from '../model/Storage';
 
 interface Props {
   children: ReactNode,
 }
 
 export const ThemeProvider = ({children, ...props}: Props) => {
-  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+  const [theme, setTheme] = useState<Theme>(
+    storage.getItem('theme') || Theme.LIGHT
+  );
+  changeSccRootVariables(theme);
   
   function changeTheme(theme: Theme) {
+    storage.setItem('theme', theme);
     setTheme(theme);
     changeSccRootVariables(theme);
   }
